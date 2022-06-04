cb4377bfc24bb34771a7680fa6b6fb666a4e26bb
Akshay Birajdar <akshaybirajdar05@gmail.com>
2022-06-05 01:28:24 +0530

    Make js build watch compatible

--- esbuild.js
+++ esbuild.js
@@ -5,13 +5,20 @@ const pagyPath = execSync("echo $(bundle show 'pagy')/lib/javascripts")
   .toString()
   .replace(/\n$/, "");
 
+const args = process.argv.slice(2);
+
+let shouldWatch = false;
+if (args[0] == "--watch") {
+  shouldWatch = true;
+}
+
 require("esbuild")
   .build({
     entryPoints: ["./app/javascript/application.js"],
     bundle: true,
     outdir: "./app/assets/builds",
     sourcemap: true,
-    // watch: true,
+    watch: shouldWatch,
     nodePaths: [pagyPath],
     minify: true,
     target: "es2015",
