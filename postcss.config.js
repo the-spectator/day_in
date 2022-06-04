085222dfa6be917a4ba2740b8debf6fc6590d82c
Akshay Birajdar <akshaybirajdar05@gmail.com>
2022-06-04 18:24:50 +0530

    #1 Make sure pagy css is getting loaded

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   plugins: [
-    require("postcss-nesting"),
+    require("postcss-import"),
+    // Required to use @apply & @screen directives. https://tailwindcss.com/docs/using-with-preprocessors#nesting
+    require("tailwindcss/nesting")(require("postcss-nesting")),
     require("autoprefixer"),
     require("tailwindcss"),
-    require("postcss-import"),
   ],
 };
