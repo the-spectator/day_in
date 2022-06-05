ee5e85ff8a50b40b86db19bc7d0e61363ed1af91
Akshay Birajdar <akshaybirajdar05@gmail.com>
2022-06-05 15:37:07 +0530

    #1 Add view_component library

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     "./app/helpers/**/*.rb",
+    "./app/components/**/*.{rb,erb,haml,html,slim}",
     "./app/javascript/**/*.js",
     "./app/views/**/*.{erb,haml,html,slim}",
   ],
