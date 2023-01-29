6c984bcae56a35cc9afe063e8023355f4c103fae
Akshay Birajdar <akshaybirajdar05@gmail.com>
2023-01-29 23:02:27 +0530

    Initalize rodauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  private
+
+  def current_account
+    rodauth.rails_account
+  end
+  helper_method :current_account
+
+  def authenticate
+    rodauth.require_account # redirect to login page if not authenticated
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  before_action :authenticate
+
   def index
+    # binding.debugger
     flash[:alert] = {
       title: 'Discussion moved',
       body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.'
